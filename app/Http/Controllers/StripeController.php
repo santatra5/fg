1f6efd282565169aa18bbcc211c9a2694e877be1
anjara <5antatra53xgmail.com>
2023-04-07 13:35:32 +0300

    stripe

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+use Stripe\Charge;
+use Stripe\Stripe;
+
+use Illuminate\Http\Request;
+
+class StripeController extends Controller
+{
+    public function index(){
+        return view('welcome');
+
+    }
+    public function checkout(){
+        Stripe::setApiKey(config('services.stripe.secret'));
+    }
+    public function success(){
+        return view('welcome');
+    }
+}
